73a8104c734d25b539682507436e495f2870313d
Benny Chew <minghuibenny@gmail.com>
2024-04-08 14:26:57 +0200

    Disabled output export mode

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "export",
-  basePath: "/interbikes",
-  trailingSlash: true,
-  skipTrailingSlashRedirect: true,
 }
 
 export default nextConfig
