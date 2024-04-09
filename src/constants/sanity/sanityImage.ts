9f9dbf0395c1d296c32b589c303f5f8de1da008a
Benny Chew <minghuibenny@gmail.com>
2024-04-09 10:35:45 +0200

    Try to fix deploy error

--- src/constants/sanity/sanityImage.ts
+++ src/constants/sanity/sanityImage.ts
@@ -1,7 +1,29 @@
 import createImageUrlBuilder from "@sanity/image-url"
 import type { Image } from "sanity"
 
-import { dataset, projectId } from "./sanityEnv"
+// import { dataset, projectId } from "./sanityEnv"
+
+export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2024-04-01"
+
+export const dataset = assertValue(
+  process.env.NEXT_PUBLIC_SANITY_DATASET,
+  "Missing environment variable: NEXT_PUBLIC_SANITY_DATASET"
+)
+
+export const projectId = assertValue(
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  "Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID"
+)
+
+export const useCdn = false
+
+function assertValue<T>(v: T | undefined, errorMessage: string): T {
+  if (v === undefined) {
+    throw new Error(errorMessage)
+  }
+
+  return v
+}
 
 const imageBuilder = createImageUrlBuilder({
   projectId: projectId || "",
